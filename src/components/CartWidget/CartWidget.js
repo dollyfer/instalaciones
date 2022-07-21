0c849936d28a4fac403591573943bf9328b62dda
Dolly Fernandez <dollyfernandezrc@gmail.com>
2022-07-20 21:03:44 -0400

    cart

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import {HiOutlineShoppingCart} from 'react-icons/hi'
-import './CartWidget.css'
+import React from "react";
+import { HiOutlineShoppingCart } from "react-icons/hi";
+import "./CartWidget.css";
+import { Link } from "react-router-dom";
 
 const CartWidget = () => {
   return (
-    <div className='carrt'>
+    <div className="carrt">
+      <Link to="/carrito">
         <HiOutlineShoppingCart></HiOutlineShoppingCart>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default CartWidget
+export default CartWidget;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import logo from '../../Images/nailswithdo.png'
-import './Logo.css'
+import React from "react";
+import logo from "../../Images/nailswithdo.png";
+import "./Logo.css";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <img className='imglogo' src={logo}></img>
-  )
-}
+    <Link to="/">
+      <img className="imgLogo" src={logo} alt= "imagenDeLogo"></img>
+    </Link>
+  );
+};
 
-export default Logo
+export default Logo;
